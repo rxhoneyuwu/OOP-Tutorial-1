205461491958e9333de01b18b94a7e74a18756c1
rxhoneyuwu <rahuulnair26@gmail.com>
2026-06-30 06:59:43 +0000

    Tutorial 4

---
+++ Java_Tutorial_4/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+
+        s1.setStudentID("CU12345");
+        s1.setName("Ali");
+        s1.setCGPA(3.75);
+        s1.setProgramme("BIT");
+
+        System.out.println("Student ID : " + s1.getStudentID());
+        System.out.println("Name       : " + s1.getName());
+        System.out.println("CGPA       : " + s1.getCGPA());
+        System.out.println("Programme  : " + s1.getProgramme());
+    }
+}
